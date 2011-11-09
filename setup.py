5894f83a549c61d5e5fc92c7e8e3b898c75eef17
Tim Wintle <timwintle@gmail.com>
2011-11-09 14:27:39 +0000

    FreeBSD support

--- setup.py
+++ setup.py
@@ -1,8 +1,17 @@
 # -*- coding: utf-8 -*-
 from distutils.core import setup, Extension
 
+import sys
+include_dirs = []
+libraries = []
+lib_dirs = []
+if sys.platform.startswith('freebsd'):
+    include_dirs.append('/usr/local/include')
+    libraries.append('iconv')
+    lib_dirs.append('/usr/local/lib')
+
 setup (name = "iconv",
-       version = "1.0",
+       version = "1.1",
        description = "iconv-based Unicode converter",
        author = "Martin v. Löwis",
        author_email = "martin@v.loewis.de",
@@ -15,6 +24,9 @@ all iconv-provided encodings.
 """,
 
        py_modules = ['iconvcodec'],
-       ext_modules = [Extension("iconv",sources=["iconvmodule.c"])]
+       ext_modules = [Extension("iconv",sources=["iconvmodule.c"],
+            include_dirs=include_dirs,libraries=libraries,
+            library_dirs=lib_dirs,
+            runtime_library_dirs=lib_dirs),]
        )
 
